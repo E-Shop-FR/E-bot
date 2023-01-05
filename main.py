a3c3dc05e15d891ab19d4cd77b3a4b9b171f2934
Thesuicidaul <hugo.ic62@gmail.com>
2023-01-05 09:08:24 +0100

    encore un test

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -7,6 +10,12 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print("Ready !")
+    print(f'Tu est connectez sous le compte {client.user}')
 
-client.run("MTA1MTIwMjUxMDI5MzA1NzY0Nw.GviMZU.SRCECZQ8H5ezfW7LVOMszIpM4gQ48GPlka8H0w")
+
+@bot.commands()
+async def test(ctx,arg):
+    await ctx.send(arg)
+
+
+bot.run("MTA1MTIwMjUxMDI5MzA1NzY0Nw.GviMZU.SRCECZQ8H5ezfW7LVOMszIpM4gQ48GPlka8H0w")
